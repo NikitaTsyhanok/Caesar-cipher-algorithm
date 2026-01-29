26107a8cf8074f061fd4efe66d8886181fa0762f
Nikita Tsyhanok <nikitatzyganok2605@gmail.com>
2026-01-29 20:14:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,27 @@
 #include <string>
 
 using namespace std;
+const int abc = 26;
 
-string caesarEncrypt(const string& text, int shift) {
+
+string caesarEncrypt(const string text, int shift) { 
     string result = text;
-    shift = shift % 26;
+    shift = shift % abc;
 
     for (int i = 0; i < result.length(); i++) {
         char c = result[i];
 
         if (c >= 'A' && c <= 'Z') {
-            result[i] = 'A' + (c - 'A' + shift + 26) % 26;
+            result[i] = 'A' + (c - 'A' + shift + abc) % abc;
         }
         else if (c >= 'a' && c <= 'z') {
-            result[i] = 'a' + (c - 'a' + shift + 26) % 26;
+            result[i] = 'a' + (c - 'a' + shift + abc) % abc;
         }
     }
     return result;
 }
 
-string caesarDecrypt(const string& text, int shift) {
+string caesarDecrypt(const string text, int shift) {
     return caesarEncrypt(text, -shift);
 }
 
@@ -28,17 +30,16 @@ int main() {
     string text;
     int shift;
 
-    cout << "Enter the text: ";
+    cout << "enter text: ";
     getline(cin, text);
 
-    cout << "Enter the offset: ";
+    cout << "enter the offset: ";
     cin >> shift;
 
     cout << "Encrypted text: "
         << caesarEncrypt(text, shift) << endl;
 
-    cout << "Decoded text: "
+    cout << "Decrypted text: "
         << caesarDecrypt(caesarEncrypt(text, shift), shift) << endl;
 
-    return 0;
 }
